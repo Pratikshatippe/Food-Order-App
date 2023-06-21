d7170c3590db0aefd9a8262b641209afc048eb6e
Pratiksha Tippe <54614091+Pratikshatippe@users.noreply.github.com>
2023-06-21 10:55:42 +0530

    Disaplyed different cards details using props

--- food_order_app/src/Component/ResCard.js
+++ food_order_app/src/Component/ResCard.js
@@ -1,15 +1,15 @@
 import './ResCard.css';
 
-const ResCard = () =>{
+const ResCard = (props) =>{
     return(
         <div className="card">
             <div className="food-img">
                 <img className="food-img" src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"></img>
             </div>
-            <h3>Pratiksha's kitchen</h3>
-            <h4>Bangalore, Pune</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 mins</h4>
+            <h3>{props.name}</h3>
+            <h4>{props.location}</h4>
+            <h4>{props.rating}</h4>
+            <h4>{props.time}</h4>
         </div>
     )
 }
--- food_order_app/src/Component/RestaurantCards.js
+++ food_order_app/src/Component/RestaurantCards.js
@@ -6,15 +6,15 @@ const RestaurantCards = () =>{
         <div className="restaurant-cards">
             <div className="search">Search</div>
             <div className="res-card">
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
+                <ResCard name="Gavran Misal" location="kolhapur, karad" rating="4.5 stars" time="20 mins"/>
+                <ResCard name="Vada Sankruti" location="Delhi, karad" rating="4.1 stars" time="40 mins"/>
+                <ResCard name="Kshatriya Hotel" location="kolhapur, prayagraj" rating="3.8 stars" time="50 mins"/>
+                <ResCard name="Shri Ram Ice-cream" location="Pune, karad" rating="3.9 stars" time="30 mins"/>
+                <ResCard name="Jasawant Sweets" location="kolhapur, karad" rating="3.5 stars" time="10 mins"/>
+                <ResCard name="Quaswa Hills" location="Sangali, karad" rating="4.1 stars" time="20 mins"/>
+                <ResCard name="Krishna Hotel" location="Satara, Thane" rating="4.2 stars" time="60 mins"/>
+                <ResCard name="Sarathi Restaurant" location="kolhapur, karad" rating="3.5 stars" time="40 mins"/>
+                <ResCard name="Zoraba Hotel" location="kolhapur, karad" rating="4.3 stars" time="20 mins"/>
             </div>
         </div>
 )

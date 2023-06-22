cd9373400af7288a702b0a56a20d088e786c902e
Pratiksha Tippe <54614091+Pratikshatippe@users.noreply.github.com>
2023-06-22 12:19:03 +0530

    Top rated and search both are working fine

--- food_order_app/src/Component/RestaurantCards.js
+++ food_order_app/src/Component/RestaurantCards.js
@@ -8,33 +8,34 @@ const RestaurantCards = () =>{
     //const [resList, setList] = useState(restaurantList);  from mockdata data is coming
 
     const [resList, setList] = useState([]);
+    const [searchResList, setSearchResList] = useState([]);
     const [searchInputValue, setSearchInputValue] = useState("");
+    const [searchDataNotPresent,setsearchDataNotPresent] = useState(false);
     
     const setTopRatedRestaurants = () =>{
         const filteredList = resList.filter(listData=>listData.data.avgRating > 4);
-        setList(filteredList);
-    }
-
-    const setSearchInputHandler = (searchData) =>{
-        setSearchInputValue(searchData);
+        setSearchResList(filteredList);
     }
 
     const filterSearchHandler = () =>{
         if(searchInputValue !== ""){
             const filteredSearchList = resList.filter(elem=>elem.data.name.toLowerCase().includes(searchInputValue.toLowerCase()));
-            setList(filteredSearchList);
+            filteredSearchList.length === 0 ? setsearchDataNotPresent(true):setSearchResList(filteredSearchList);
+            setSearchInputValue("");
         }
         else{
-            setList(resList);
+            setSearchResList(resList);
+            setsearchDataNotPresent(false);
         }
     }
     
     const fetchData = async() =>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7049873&lng=74.24325270000001&sortBy=RELEVANCE&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        console.log(json);
         //Optional Chaining
-        setList(json?.data?.cards[2]?.data?.data?.cards);
+        const fetchedData = json?.data?.cards[2]?.data?.data?.cards;
+        setList(fetchedData);
+        setSearchResList(fetchedData);
     }
     useEffect(()=>{
         fetchData();
@@ -44,13 +45,13 @@ const RestaurantCards = () =>{
         <div className="restaurant-cards">
             <div className="filter">
                 <button className="filter-btn" onClick={setTopRatedRestaurants}>Top Rated Restaurants</button>
-                <input type="text" className='search-input' placeholder='Enter name of dish which you want to eat now'onChange={(e)=>setSearchInputHandler(e.target.value)}/>
+                <input type="text" className='search-input' value={searchInputValue} placeholder='Enter name of dish which you want to eat now'onChange={(e)=>setSearchInputValue(e.target.value)}/>
                 <button className='search-btn' onClick={filterSearchHandler}>Search</button>
             </div>
             <div className="res-card">{
-                resList.length === 0 ? <Shimmer/>:
-                resList.map(list=>(
-                <ResCard key={list.data.id} resData = {list}/>))}
+                searchDataNotPresent ? "Data Not Found" : (searchResList.length === 0 ? <Shimmer/>:
+                searchResList.map(list=>(
+                <ResCard key={list.data.id} resData = {list}/>)))}
             </div>
         </div>
 )

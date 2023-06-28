3e851860a993e32ac0e593ed59241204d614eacb
Pratiksha Tippe <54614091+Pratikshatippe@users.noreply.github.com>
2023-06-28 21:52:09 +0530

    componentDidMount functionality working fine

--- food_order_app/src/pages/Aboutus.js
+++ food_order_app/src/pages/Aboutus.js
@@ -1,10 +1,11 @@
 import UserClass from "./UserClass";
 
 const Aboutus = () =>{
+
     return(        
         <div className="about">
             <h1>This is all about the company</h1>
-            <UserClass name={"Pratiksha Tippe"} location={"Kolhapur"} role={"Sr. Software Developer"}/>
+            <UserClass/>
         </div>
 
     )
--- food_order_app/src/pages/UserClass.js
+++ food_order_app/src/pages/UserClass.js
@@ -4,15 +4,29 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state={
+            userInfo:{
+                name:"PT",
+                location:"Pune"
+            },
             count:0
         }
     }
+
+    componentDidMount=async()=>{
+        const data = await fetch("https://api.github.com/users/Pratikshatippe");
+        const json = await data.json();
+        console.log(json)
+        this.setState({
+            userInfo:json
+        })
+    }
     
     render(){
-        const {name, location, role} = this.props;
+        const {name, location,avatar_url} = this.state.userInfo;
 
         return(
             <div className="user-card">
+                <img src={avatar_url}></img>
                 <h1>count = {this.state.count}</h1>
                 <button className="primary-btn" onClick={()=>
                     this.setState({
@@ -21,7 +35,6 @@ class UserClass extends React.Component{
                 }>Increment</button>
                 <h2>{name}</h2>
                 <h3>{location}</h3>
-                <h3>{role}</h3>
             </div>
         )
     }

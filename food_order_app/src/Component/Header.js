3944c985668578f9d09360203087411eeb8b9c46
Pratiksha Tippe <54614091+Pratikshatippe@users.noreply.github.com>
2023-07-03 11:23:28 +0530

    header css changed

--- food_order_app/src/Component/Header.js
+++ food_order_app/src/Component/Header.js
@@ -1,4 +1,4 @@
-import './Header.css';
+//import './Header.css';
 import { LOGO_URL } from '../Utils/config';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -14,21 +14,23 @@ const Header = () =>{
         console.log("inside useEffect hook");
     },[loginButton])
 return(
-    <div className="header">
-        <div className="logo">
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 text-white">
+    <div className="header flex justify-between">
+        <div className="logo w-36 p-3">
             <img alt="header-logo-img" className="header-logo-img" src={LOGO_URL}></img>
         </div>
-        <div className="nav-item">
-            <ul>
-                <li><Link to="/" className='text-link'>Home</Link></li>
-                <li><Link to="/about" className='text-link'>About Us</Link></li>
-                <li><Link to="/contact" className='text-link'>Contact Us</Link></li>
-                <li><Link to="/grocery" className='text-link'>Grocery</Link></li>
-                <li>Cart</li>
-                <button className='login' onClick={loginHandler}>{loginButton}</button>
+        <div className="nav-item m-7">
+            <ul className='flex'>
+                <li className='p-2 text-3xl'><Link to="/" className='text-link'>Home</Link></li>
+                <li className='p-2 text-3xl'><Link to="/about" className='text-link'>About Us</Link></li>
+                <li className='p-2 text-3xl'><Link to="/contact" className='text-link'>Contact Us</Link></li>
+                <li className='p-2 text-3xl'><Link to="/grocery" className='text-link'>Grocery</Link></li>
+                <li className='p-2 text-3xl'>Cart</li>
+                <button className='login p-2 text-2xl bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={loginHandler}>{loginButton}</button>
             </ul>
         </div>
     </div>
+    </nav>
 )
 }
 

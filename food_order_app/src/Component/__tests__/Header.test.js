5c58eba9608cddd76d7014d24bb4596ae35919c8
Pratiksha Tippe <54614091+Pratikshatippe@users.noreply.github.com>
2023-07-11 22:59:16 +0530

    Logo test case

---
+++ food_order_app/src/Component/__tests__/Header.test.js
@@ -0,0 +1,7 @@
+// import { render } from "react-dom"
+// import Header from "../Header"
+
+// test("load header logo ",()=>{
+//     const header = render(<Header/>);
+//     console.log(header);
+// })
